2b21c71e0512f7012b9ef1f474d245e3591ba93b
Mann Mehta <44433995+mann2108@users.noreply.github.com>
2020-01-17 01:06:33 +0530

    Create BFTT.cpp

---
+++ CODECHEF/BFTT.cpp
@@ -0,0 +1,59 @@
+
+/* HEADER DECLARATION BLOCK */
+
+#include<bits/stdc++.h>
+#include<cstdio>
+
+
+using namespace std;
+
+/* CONSTANT DECALRATION BLOCK */
+#define mod 10000007
+
+/* ALIAS CREATION BLOCK */
+
+typedef long long int ll;
+typedef long double ld;
+typedef unsigned long long int ull;
+
+
+/* START OF MAIN */
+int main(){
+
+
+ll test_cases;
+cin>>test_cases;
+
+
+while(test_cases--){
+    ll n;
+    cin>>n;
+    ll temp=n;
+    ll cnt3=0;
+    ll counter=n+1;
+    temp=counter;
+    while(true){
+
+         for(ll i=0;temp!=0;i++){
+            if(temp%10==3)
+                cnt3++;
+            temp=temp/10;
+         }
+
+
+          if(cnt3>=3){
+            break;
+          }
+         counter++;
+         temp=counter;
+         cnt3=0;
+    }
+    cout<<counter<<endl;
+
+
+}
+return 0;
+
+
+/* END OF MAIN */
+}
