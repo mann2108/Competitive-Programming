2992fb387d34fc3435caedc3b5d76a2eacdec05c
Mann Mehta <44433995+mann2108@users.noreply.github.com>
2020-01-17 01:06:22 +0530

    Create SMPAIR.cpp

---
+++ CODECHEF/SMPAIR.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    long int t;
+    cin>>t;
+    long int n;
+    while(t!=0)
+    {
+        int temp;
+        cin>>n;
+        long int a[n];
+            for(long long int i=0; i<n; ++i)
+            {
+                cin>>a[i];
+            }
+        if(n!=1)
+        {
+            sort(a,a+n);
+            temp=a[0]+a[1];
+        }
+        else{
+            temp=a[0];
+        }
+        cout<<temp<<endl;
+        t--;
+    }
+
+
+}
+
+
