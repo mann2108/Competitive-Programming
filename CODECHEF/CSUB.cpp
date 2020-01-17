1eabd5b05a4b9141ea86462d07cc36ba0a6d8424
Mann Mehta <44433995+mann2108@users.noreply.github.com>
2020-01-17 19:21:37 +0530

    Create CSUB.cpp

---
+++ CODECHEF/CSUB.cpp
@@ -0,0 +1,37 @@
+/*
+
+Name:- Mann Mehta
+Codechef ID:- mann2108
+
+problem code:- CSUBS
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+inline bool sorted(const ll x,const ll y){return x>y;}
+
+int main(){
+    ll test;
+    cin>>test;
+    while(test--){
+        ll n;
+        cin>>n;
+        string s;
+        cin>>s;
+        ll cnt=0;
+        for(ll i=0;i<s.length();i++){
+            if(s[i]=='1'){
+                cnt++;
+            }
+        }
+        ll ans=cnt+(((cnt)*(cnt-1))/2);
+        cout<<ans<<endl;
+    }
+
+}
+
+
+
