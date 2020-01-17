78ed21167cbc8b42c507509b59c5f522bd6a2492
Mann Mehta <44433995+mann2108@users.noreply.github.com>
2020-01-17 19:21:12 +0530

    Create TEST.cpp

---
+++ CODECHEF/TEST.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+bool flag=true;
+while(flag){
+long long int n;
+cin>>n;
+if(n==42){
+flag=false;
+
+}
+if(flag)
+cout<<n<<endl;
+
+}
+}
