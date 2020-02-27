0172ebe65243e12e4edb070b0ee20f32a70d8553
Mann Mehta <44433995+mann2108@users.noreply.github.com>
2020-02-27 12:13:29 +0530

    Update NAICHEF.cpp

--- CODECHEF/NAICHEF.cpp
+++ CODECHEF/NAICHEF.cpp
@@ -2,25 +2,42 @@
 #include<stdio.h>
 using namespace std;
 int main(){
-    long int t,x,y,n;
-    cin>>t;
-    while(t!=0){
-        cin>>n>>x>>y;
-        int cnt1=0,cnt2=0;
-        long int a[n];
-        for(long int i=0;i<n;i++){
-            cin>>a[i];
-            if(x==a[i]){
-                cnt1++;
-            }
-            if(y==a[i]){
-                cnt2++;
-            }
+long int t,x,y,n;
+cin>>t;
+while(t!=0){
+    cin>>n>>x>>y;
+    int cnt1=0,cnt2=0;
+    long int a[n];
+    for(long int i=0;i<n;i++){
+        cin>>a[i];
+        if(x==a[i]){
+            cnt1++;
         }
-        float f=((cnt1*cnt2)/(float)(n*n));
-        f=((float)cnt/(n*n));
-        printf("%0.10f \n",f);
-        t--;
+        if(y==a[i]){
+            cnt2++;
+        }
     }
-}
+    float f=((cnt1*cnt2)/(float)(n*n));
 
+
+
+
+
+    /*
+
+    int cnt=0;
+    for(long int i=0;i<n;i++){
+        for(long int j=0;j<n;j++){
+            if((a[i]==x)&&(a[j]==y))
+            {
+                 cnt++;
+                 break;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                       }
+         }
+    }
+    float f;
+    f=((float)cnt/(n*n));*/
+    printf("%0.10f \n",f);
+  t--;
+}
+}
