e6fad24c8cf3a3535a1a9bcce822e758fb6adb9d
Mann Mehta <44433995+mann2108@users.noreply.github.com>
2020-02-27 12:11:19 +0530

    Create WEFRNDS.cpp

---
+++ CODECHEF/WEFRNDS.cpp
@@ -0,0 +1,20 @@
+/** mann2108 **/
+
+/** Problem Code:- WEFRNDS **/
+
+/** Based SOCIAL NETWORK's formed by network of 
+friendship in class followed by patterns. **/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main(){
+ ll t;
+ cin>>t;
+ while(t--){
+  ll n;
+  cin>>n;
+  cout<<n/2<<endl;
+ }
+}
