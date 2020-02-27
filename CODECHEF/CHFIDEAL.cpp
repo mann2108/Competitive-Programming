d697db881ed9a94e7233cc90bc54d2487db35cd6
Mann Mehta <44433995+mann2108@users.noreply.github.com>
2020-02-27 12:16:51 +0530

    Create CHFIDEAL.cpp

---
+++ CODECHEF/CHFIDEAL.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cstdio>
+#include<stdlib.h>
+using namespace std;
+typedef long long int ll;
+int main(){
+
+//if(n==1){
+    cout<<"1"<<endl;
+    int n;
+    cin>>n;
+    if(n==2){
+        cout<<"3"<<endl;
+    }
+    else{
+    cout<<"2"<<endl;
+    }
+    
+   
+  //  cout<<"3"<<endl;
+
+
+}
