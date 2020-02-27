750a32110dd7dc49d4a034ad064a1cb14324da06
Mann Mehta <44433995+mann2108@users.noreply.github.com>
2020-02-27 12:16:38 +0530

    Create CHSERVE.cpp

---
+++ CODECHEF/CHSERVE.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main(){
+ll t;
+cin>>t;
+while(t--){
+    ll x,y,z;
+    cin>>x>>y>>z;
+    ll q=x+y;
+    bool flag;
+    if(((q/z)%2)==0){
+        cout<<"CHEF"<<endl;;
+    }
+    else{
+        cout<<"COOK"<<endl;
+    }
+
+
+
+}
+
+}
