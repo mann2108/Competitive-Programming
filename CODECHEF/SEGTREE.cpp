8e2da4dea31475f3a748bf0ae907b502fc60f6a6
Mann Mehta <44433995+mann2108@users.noreply.github.com>
2020-02-27 12:12:00 +0530

    Create SEGTREE.cpp

---
+++ CODECHEF/SEGTREE.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+typedef long long int ll;
+int main() {
+ ll t;
+ cin>>t;
+ while(t--){
+     ll n;
+     cin>>n;
+     cout<<n+(n-1)<<endl;
+ }
+ return 0;
+}
