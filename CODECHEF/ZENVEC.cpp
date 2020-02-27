24df081a70f98ab394cf01d1d29efb9b86f7914b
Mann Mehta <44433995+mann2108@users.noreply.github.com>
2020-02-27 12:11:11 +0530

    Create ZENVEC.cpp

---
+++ CODECHEF/ZENVEC.cpp
@@ -0,0 +1,23 @@
+/** AUTHOR:- mann2108 **/
+
+/** Problem Code:- ZENVEC **/
+
+/** Based On Zeno's Detchomy Paradox **/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main(){
+ ll t;
+ cin>>t;
+ while(t--){
+  ll s,d;
+  cin>>s>>d;
+  if(s==d){
+   cout<<0<<endl;
+  }
+  else{
+   cout<<-1<<endl;
+  }
+ }
+}
